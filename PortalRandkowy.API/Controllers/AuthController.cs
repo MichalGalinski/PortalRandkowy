ac941c1649fed6f6c3f5c0d7ff2e8bfee26c7115
unknown <michal.galinski1@gmail.com>
2019-07-16 19:44:46 +0200

    Create Token

--- PortalRandkowy.API/Controllers/AuthController.cs
+++ PortalRandkowy.API/Controllers/AuthController.cs
@@ -1,5 +1,11 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using PortalRandkowy.API.Data;
 using PortalRandkowy.API.Dtos;
 using PortalRandkowy.API.Models;
@@ -11,23 +17,51 @@ namespace PortalRandkowy.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository repository;
-        public AuthController(IAuthRepository repository)
+        private readonly IConfiguration config;
+        public AuthController(IAuthRepository repository, IConfiguration config)
         {
+            this.config = config;
             this.repository = repository;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register (UserForRegisterDto userForRegisterDto)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
-            if(await this.repository.UserExist(userForRegisterDto.Username))
+            if (await this.repository.UserExist(userForRegisterDto.Username))
                 return BadRequest("Użytownik o takiej nazwie już istnieje !");
             var userToCreate = new User
             {
                 UserName = userForRegisterDto.Username
             };
-            var createdUser = await this.repository.Register(userToCreate,userForRegisterDto.Password);
+            var createdUser = await this.repository.Register(userToCreate, userForRegisterDto.Password);
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForRegisterDto)
+        {
+            var userFromRepo = await this.repository.Login(userForRegisterDto.Username.ToLower(), userForRegisterDto.Password);
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            //creat Token
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name,userFromRepo.UserName)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddHours(12),
+                SigningCredentials = creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new {token = tokenHandler.WriteToken(token)});
         }
     }
 }
