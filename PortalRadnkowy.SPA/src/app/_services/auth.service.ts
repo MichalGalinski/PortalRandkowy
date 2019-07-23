1e828ddfbf89810bbaf88cffa69e47780d29056c
unknown <michal.galinski1@gmail.com>
2019-07-23 21:31:34 +0200

    Personalizacja

--- PortalRadnkowy.SPA/src/app/_services/auth.service.ts
+++ PortalRadnkowy.SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,20 @@ import {map} from 'rxjs/operators';
 export class AuthService {
 
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
+
 
 constructor(private http: HttpClient) { }
 
-login(model: any){
+login(model: any) {
   return this.http.post(this.baseUrl + 'login' , model)
   .pipe(map((respone: any) => {
     const user = respone;
     if (user) {
       localStorage.setItem('token', user.token);
+      this.decodedToken = this.jwtHelper.decodeToken(user.token);
+      console.log(this.decodedToken);
     }
   }));
 }
@@ -24,4 +30,10 @@ login(model: any){
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
 }
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
+}
+
 }
