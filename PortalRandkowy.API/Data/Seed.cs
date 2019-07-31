ca21d56cba5c060ec04714a0940cfeadfb5195cb
unknown <michal.galinski1@gmail.com>
2019-07-31 21:55:47 +0200

    Ładowanie danych testowych do bazy danych.

---
+++ PortalRandkowy.API/Data/Seed.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using PortalRandkowy.API.Models;
+
+namespace PortalRandkowy.API.Data
+{
+    public class Seed
+    {
+        private readonly DataContext context;
+        public Seed(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public void SeedUsers()
+        {
+            if(!this.context.Users.Any())
+            {
+                var userData = File.ReadAllText("Data/UserSeedData.json");
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+                foreach(var user in users)
+                {
+                    byte[] passwordHash, passwordSalt;
+                    CreatePasswordHashSalt("password", out passwordHash, out passwordSalt);   
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+                    user.UserName = user.UserName.ToLower();
+                    this.context.Users.Add(user);
+                }
+                this.context.SaveChanges();
+            }            
+        }
+
+        private void CreatePasswordHashSalt(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }           
+        } 
+    }
+}
